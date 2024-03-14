9cd6d658bf91a1ec6c3f7ddd73dbc11f506f0bb1
Bima Setyo <bima030201@gmail.com>
2024-03-14 16:10:35 +0700

    update header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,17 +1,9 @@
 import React from "react";
-import { MdMenu, MdAccountCircle } from "react-icons/md";
 
 export const Header = () => {
   return (
-    <div className='bg-gray-100 h-12 flex items-center justify-between sticky top-0'>
-      <div className='ml-4 mr-6'>
-        <MdMenu size={24} />
-      </div>
-      <div className='text-lg'>Tada Todo</div>
-      {/* add user image on right */}
-      <div className='ml-6 mr-4'>
-        <MdAccountCircle size={24} />
-      </div>
+    <div className='bg-gray-100 h-12 sticky top-0'>
+      <div className='text-lg mx-auto'>Tada Todo</div>
     </div>
   );
 };
