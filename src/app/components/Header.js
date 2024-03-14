fff9bb7b4c8895988a8438daca72b421d53eeac9
Bima Setyo <bima030201@gmail.com>
2024-03-14 16:17:45 +0700

    update header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <div className='bg-gray-100 h-12 sticky top-0'>
+    <div className='bg-gray-100 h-12 flex items-center sticky top-0'>
       <div className='text-lg mx-auto'>Tada Todo</div>
     </div>
   );
